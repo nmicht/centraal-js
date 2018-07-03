ca557be0999b443994014ceb5c2ae0f097600ba8
Chacon <a01229574@itesm.mx>
2018-07-02 21:08:49 -0500

    added tabs

--- JS-101/chacon-calculator.js
+++ JS-101/chacon-calculator.js
@@ -1,11 +1,10 @@
-<script type="text/javascript">
 /**
  * Adds two numbers and print the result
  * @param  {number} num1 First number
  * @param  {number} num2 Second number
  */
 function sum(num1, num2) {
-  console.log(num1 + num2);
+    console.log(num1 + num2);
 }
 /**
  * Substracts two numbers and print the result
@@ -13,7 +12,7 @@ function sum(num1, num2) {
  * @param  {number} num2 Second number
  */
 function minus(num1, num2) {
-  console.log(num1 - num2);
+    console.log(num1 - num2);
 }
 /**
  * Multiplies two numbers and print the result
@@ -21,7 +20,7 @@ function minus(num1, num2) {
  * @param  {number} num2 Second number
  */
 function prod(num1, num2) {
-  console.log(num1 * num2);
+    console.log(num1 * num2);
 }
 /**
  * Divides two numbers and print the result
@@ -29,7 +28,7 @@ function prod(num1, num2) {
  * @param  {number} num2 Second number
  */
 function div(num1, num2) {
-  console.log(num1 / num2);
+    console.log(num1 / num2);
 }
 
 /**
@@ -37,6 +36,5 @@ function div(num1, num2) {
  * @param  {string} num number to be squared.
  */
 function square(num) {
-  console.log(num * num);
+    console.log(num * num);
 }
-</script>
