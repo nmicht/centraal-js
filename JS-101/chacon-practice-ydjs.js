962705357abb81b819643d4ebb2bda14f6d01074
Chacon <a01229574@itesm.mx>
2018-07-02 21:53:56 -0500

    corrected errors and beautified it

--- JS-101/chacon-practice-ydjs.js
+++ JS-101/chacon-practice-ydjs.js
@@ -1,41 +1,38 @@
-var bankAccount;
-var totalPhonePrice = phonePrice()
-
-bankAccount = bankAccountAsk('How much money have you got?')
-
-
-/*****************************************Functions*****************************************/
-function bankAccountAsk(message){
-  var money;
-  do {
-    money = prompt(message);
-    if (isNaN(money)) {
-      console.error(money, 'That is not a valid amount');
-    }
-  } while (isNaN(money));
-  return money;
+function bankAccountAsk(message) {
+    var money;
+    do {
+        money = prompt(message);
+        if (isNaN(money)) {
+            console.error(money, 'That is not a valid amount');
+        }
+    } while (isNaN(money));
+    return money;
 }
 
-function buyPhone(phoneTotal, bankAccount){
-  var phonesBought = 0;
-  do {
-    phonesBought = phonesBought + 1
-  } while (Bank >= phoneTotal);
-  console.log(phonesBought)
+function buyPhone(phoneTotal, bankAccount) {
+    var phonesBought = 0;
+    do {
+        phonesBought += 1;
+    } while (Bank >= phoneTotal);
+    console.log(phonesBought);
 }
+
 function phonePrice() {
-var phone = 200;
-var tax = .16;
-var accessory = 30;
-var phoneTotal;
+    var phone = 200;
+    var tax = 0.16;
+    var accessory = 30;
+    var phoneTotal;
 
-phoneTotal = ((phone + accessory) * (1 + tax))
+    phoneTotal = ((phone + accessory) * (1 + tax));
 
-return totalPhonePrice;
-
+    return totalPhonePrice;
 }
 
 function printTicket() {
-  console.log('Thanks for your purchase')
-  console.log()
+    console.log('Thanks for your purchase');
+    console.log();
 }
+var bankAccount;
+var totalPhonePrice = phonePrice();
+
+bankAccount = bankAccountAsk('How much money have you got?');
