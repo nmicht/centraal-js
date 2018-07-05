46094a63c5495d6079de4be0728c66b448a8ae1e
Ramon Posadas Martinez <posadas_r@hotmail.com>
2018-07-04 21:53:49 -0500

    I created new functions and now it is fine

--- Objects/ball/js/ball.js
+++ Objects/ball/js/ball.js
@@ -5,12 +5,53 @@ const ball = {
         return `Hi, I'm ${this.color}, nice to meet you ${name}`;
     },
     bounceBack: function bounceBack(color = '') {
-        return `Ball ${this.color} goes up ${color}`;
+        return `${this.color} ball goes up ${color}`;
     },
     roll: function roll() {
         return 'Ball rolls in the ground';
     },
     float: function float() {
-        return 'Balls stays up in the water';
+        return 'Ball stays up in the water';
     },
 };
+
+function hiClicked() {
+    let msg = ball.sayHi();
+
+    let textarea = document.getElementById('messages');
+
+    let oldMsgs = textarea.textContent;
+
+    textarea.textContent = `${oldMsgs}\n${msg}`;
+}
+
+function bounceBack() {
+    let msg = ball.bounceBack();
+
+    let textarea = document.getElementById('messages');
+
+    let oldMsgs = textarea.textContent;
+
+    textarea.textContent = `${oldMsgs}\n${msg}`;
+}
+
+function roll() {
+    let msg = ball.roll();
+
+    let textarea = document.getElementById('messages');
+
+    let oldMsgs = textarea.textContent;
+
+    textarea.textContent = `${oldMsgs}\n${msg}`;
+}
+
+
+function float() {
+    let msg = ball.float();
+
+    let textarea = document.getElementById('messages');
+
+    let oldMsgs = textarea.textContent;
+
+    textarea.textContent = `${oldMsgs}\n${msg}`;
+}
