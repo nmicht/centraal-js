171eb08ba8f35b6aaf574f86b68599abca267ba1
Ramon Posadas Martinez <posadas_r@hotmail.com>
2018-07-04 21:04:52 -0500

    I changed all my programs to IIFES

--- JS-101/ramon-apple-store.js
+++ JS-101/ramon-apple-store.js
@@ -1,82 +1,71 @@
-"use strict";
-
-const phonePrice = 1000;
-const accesoryPrice = 500;
-const tax = 0.16;
-const spendingThreshold = 20000;
-
-
-let bankAccount = 35000;
-let phoneQuantity;
-let accesoryQuantity;
-let surcharges;
+(function () {
+    "use strict";
+    const phonePrice = 1000;
+    const accesoryPrice = 500;
+    const tax = 0.16;
+    const spendingThreshold = 20000;
 
 
-do {
-    numberPhone = phonePrice*phoneQuantity;
-    if (numberPhone <= spendingThreshold) {
-        numberAccesory = accesoryPrice*accesoryQuantity;
+    let bankAccount = 35000;
+    let phoneQuantity;
+    let accesoryQuantity;
+    let surcharges;
+
+
+    do {
+        numberPhone = phonePrice*phoneQuantity;
+        if (numberPhone <= spendingThreshold) {
+            numberAccesory = accesoryPrice*accesoryQuantity;
+        }
+    } while (numberPhone <= bankAccount);
+
+
+    surcharges = calcSurcharges(price, quantity);
+
+    printTicket(surcharges.subtotal, surcharges.tax, surcharges.total);
+
+
+    /**
+
+    * Print a ticket for a purchase including surcharges
+    *
+    * @param {number} subtotal The subtotal of the purchase
+    * @param {number} tax      Calculated tax for the purchase
+    * @param {number} total    The total amount
+    *
+    */
+
+    function printTicket(subtotal, tax, total) {
+        console.log('GRACIAS POR TU COMPRA');
+        console.log('Subtotal: $', subtotal.toFixed(2));
+        console.log('IVA: $', tax.toFixed(2));
+        console.log('Total: $', total.toFixed(2));
     }
-} while (numberPhone <= bankAccount);
-
-// Convertir a numero
 
 
-surcharges = calcSurcharges(price, quantity);
+    /**
 
-printTicket(surcharges.subtotal, surcharges.tax, surcharges.total);
+    * Calc the surcharges (subtotal, tax and total) based on price and quantity.
+    *
+    * @param {number} price     The price of the product
+    * @param {number} quantity  The amount of products to buy
+    *
+    * @return {object}          An object with the subtotal, tax and total.
+    *
+    */
 
-
-/**
-
-* Print a ticket for a purchase including surcharges
-*
-* @param {number} subtotal The subtotal of the purchase
-* @param {number} tax      Calculated tax for the purchase
-* @param {number} total    The total amount
-*
-*/
-
-function printTicket(subtotal, tax, total) {
-    console.log('GRACIAS POR TU COMPRA');
-    console.log('Subtotal: $', subtotal.toFixed(2));
-    console.log('IVA: $', tax.toFixed(2));
-    console.log('Total: $', total.toFixed(2));
-}
+    function calcSurcharges(quantity) {
+        quantity = Number(quantity);
 
 
-/**
+        let subtotal = price * quantity;
+        let tax = subtotal * 0.16;
+        let total = subtotal + tax;
 
-* Calc the surcharges (subtotal, tax and total) based on price and quantity.
-*
-* @param {number} price     The price of the product
-* @param {number} quantity  The amount of products to buy
-*
-* @return {object}          An object with the subtotal, tax and total.
-*
-*/
-
-function calcSurcharges(quantity) {
-    quantity = Number(quantity);
-
-
-    let subtotal = price * quantity;
-    let tax = subtotal * 0.16;
-    let total = subtotal + tax;
-
-    return {
-        subtotal,
-        tax,
-        total,
-    };
-}
-
-/**
-
-* Display a message and get the input from the user. Will return the input only when it is a number.
-*
-* @param {string} message     The price of the product
-*
-* @return {number}          ....
-*
-*/
+        return {
+            subtotal,
+            tax,
+            total,
+        };
+    }
+}());
--- JS-101/ramon-serie.js
+++ JS-101/ramon-serie.js
@@ -1,19 +1,21 @@
-"use strict";
-/**
-Serie del uno al diez en consola
-*/
+(function () {
+    "use strict";
+    /**
+    Serie del uno al diez en consola
+    */
 
-let numero;
-let counter = 1;
+    let numero;
+    let counter = 1;
 
-do {
-    numero = prompt('Escribe un numero mayor a 1');
-    if (isNaN(numero) || numero <= 1) {
-        console.error('El valor', numero, 'no es valido');
+    do {
+        numero = prompt('Escribe un numero mayor a 1');
+        if (isNaN(numero) || numero <= 1) {
+            console.error('El valor', numero, 'no es valido');
+        }
+    } while (isNaN(numero) || numero <= 1);
+
+
+    for (counter; counter <= numero; counter += 1) {
+        console.log(counter);
     }
-} while (isNaN(numero) || numero <= 1);
-
-
-for (counter; counter <= numero; counter += 1) {
-    console.log(counter);
-}
+}());
