4f416e6ca293b210bc4248a022be910b19dc54c5
georgia andrea pinal arana <pinal.andrea@gmail.com>
2018-07-04 21:58:53 -0500

    more functions to aircon same process

--- Objects/airCon/airCon.js
+++ Objects/airCon/airCon.js
@@ -4,9 +4,28 @@ let airCon = {
         return `Hi I'm ${this.name} You are nice ${name}`;
     },
     getCold: function getCold() {
-        return 'freezing';
+        return 'freezing, I\'m freezing \nbrrrrrrrrrrrrr\n ';
     },
     getHot: function getHot() {
-        return 'hotting';
+        return 'it\'s getting hot in herrre';
     },
 };
+
+function HiClicked() {
+    let msg = airCon.sayHi();
+    let textarea = document.getElementById('messages');
+    let oldMsgs = textarea.textContent;
+    textarea.textContent = oldMsgs + '' + msg;
+}
+function FreezeClicked() {
+    let msg = airCon.getCold();
+    let textarea = document.getElementById('messages');
+    let oldMsgs = textarea.textContent;
+    textarea.textContent = oldMsgs + '' + msg;
+}
+function HotClicked() {
+    let msg = airCon.getHot();
+    let textarea = document.getElementById('messages');
+    let oldMsgs = textarea.textContent;
+    textarea.textContent = `${oldMsgs}\n${msg}`;
+}
