dddf0e0ad287561782dd93891c18a12b899be34a
Chacon <a01229574@itesm.mx>
2018-07-05 21:51:00 -0500

    created label and input
    with working error

--- Form/js/validate.js
+++ Form/js/validate.js
@@ -2,3 +2,10 @@ const inputCosa = document.getElementById('cosa');
 inputCosa.addEventListener('focus', function () {
     this.className = 'focus';
 });
+
+let inputName = document.getElementById('name');
+inputName.addEventListener('blur', function () {
+    if (inputName.value == false) {
+        this.className = 'error';
+    }
+});
