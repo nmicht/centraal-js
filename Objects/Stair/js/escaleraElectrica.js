807f40dc7e2bff55b543260ebb0d026f7a615043
Chacon <a01229574@itesm.mx>
2018-07-05 20:15:45 -0500

    deleted fucttions from html

--- Objects/Stair/js/escaleraElectrica.js
+++ Objects/Stair/js/escaleraElectrica.js
@@ -1,38 +1,60 @@
 'use strict';
 
-let stair = {
+const stair = {
     name: 'Stair of the future',
     numberStairs: '35',
     sayHi: function sayHi(name = '') {
         return `Greetings ${name} I'm the ${this.name}`;
     },
     getUp: function getUp(name = '') {
-        return `Lets go to the top ${name}`;
+        return `let´s go to the top ${name}`;
     },
     goDown: function goDown() {
-        return `Lets go to the bottom ${name}`;
+        return `let´s go to the bottom ${name}`;
     },
     stop: function stop() {
-        return `Lets take a break ${name}`;
+        return `let´s take a break ${name}`;
     },
     powerDown: function powerDown() {
-        return 'You can´t stop the tried to stop the future';
+        return 'You tried to stop the future, you can´t stop the future';
     },
     bye: function bye(friendo) {
         console.log('bye ', friendo);
     }
 };
 
-function hiClicked() {
-    let msg = stair.sayHi();
-    let box = document.getElementById('messages');
-    let oldBox = box.textContent;
-    box.textContent = `${oldBox} \n ${msg}`;
+let btnHi = document.getElementById('buttonHi')
+btnHi.onclick = function () {
+    const msg = stair.sayHi();
+    addMessage(msg)
 }
 
-function goUpClicked() {
-    let msg = stair.getUp();
-    let box = document.getElementById('messages');
-    let oldBox = box.textContent;
-    box.textContent = `${oldBox} \n ${msg}`;
+let btnUp = document.getElementById('buttonUp')
+btnUp.onclick = function () {
+    const msg = stair.getUp();
+    addMessage(msg)
+}
+
+let btnDown = document.getElementById('buttonDown')
+btnDown.onclick = function () {
+    const msg = stair.goDown();
+    addMessage(msg)
+}
+
+let btnStop = document.getElementById('buttonStop')
+btnStop.onclick = function () {
+    const msg = stair.stop();
+    addMessage(msg)
+}
+
+let btnPowerDown = document.getElementById('buttonPowerDown')
+btnPowerDown.onclick = function () {
+    const msg = stair.powerDown();
+    addMessage(msg)
+}
+
+function addMessage(msg) {
+    const box = document.getElementById('messages');
+    const oldBox = box.textContent;
+    box.textContent = `${oldBox}\n${msg}`;
 }
