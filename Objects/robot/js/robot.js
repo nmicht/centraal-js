15d1fb7ed4ce7f269045aa6f1983b2d2d28e9268
Michelle Torres <mtorres@tiempodevelopment.com>
2018-07-05 20:09:22 -0500

    Apply DRY for messages

--- Objects/robot/js/robot.js
+++ Objects/robot/js/robot.js
@@ -13,3 +13,19 @@ const robot = {
         return 'smoooooooooking';
     },
 };
+
+function clickHi() {
+    const msg = robot.sayHi();
+    addMessage(msg);
+}
+
+function clickSmoke() {
+    const msg = robot.smoke();
+    addMessage(msg);
+}
+
+function addMessage(msg) {
+    const textarea = document.getElementById('messages');
+    const oldMsgs = textarea.textContent;
+    textarea.textContent = `${oldMsgs}\n${msg}`;
+}
