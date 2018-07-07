7183077f25f2844537dc6e47bdd2091a00216baf
georgia andrea pinal arana <pinal.andrea@gmail.com>
2018-07-06 20:02:29 -0500

    i tried to follow and correct my select unsuccesfully

--- Form/js/validate.js
+++ Form/js/validate.js
@@ -2,3 +2,10 @@ const inputCosa = document.getElementById('cosa');
 inputCosa.addEventListener('focus', function () {
     this.className = 'focus';
 });
+const select = document.getElementById('select');
+select.addEventListener('blur', function () {
+    if (this.value === 0) {
+        console.error('lo dejaste en blanco puto');
+        this.className = 'error';
+    }
+});
