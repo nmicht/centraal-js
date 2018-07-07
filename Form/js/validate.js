3d72d212aad6d550005e26d7c3b5e777b51f971c
Michelle Torres <mtorres@tiempodevelopment.com>
2018-07-06 19:59:42 -0500

    Added elements and validation

--- Form/js/validate.js
+++ Form/js/validate.js
@@ -1,11 +1,37 @@
-const inputCosa = document.getElementById('cosa');
-inputCosa.addEventListener('focus', function () {
-    this.className = 'focus';
-});
-
 const inputName = document.getElementById('name');
 inputName.addEventListener('blur', function () {
-    if (inputName.value == false) {
+    if (this.value == false) {
         this.className = 'error';
     }
 });
+
+const something = document.getElementById('something');
+something.addEventListener('blur', function () {
+    if (this.value == 0) {
+        this.className = 'error';
+    }
+});
+
+const ageInput = document.getElementById('age');
+ageInput.addEventListener('blur', function () {
+    if (this.value < 18) {
+        this.className = 'error';
+    }
+});
+
+const submit = document.getElementById('submit');
+submit.addEventListener('click', function () {
+    let timeM = document.getElementById('time-m');
+    let timeT = document.getElementById('time-t');
+    let timeN = document.getElementById('time-n');
+    if(!timeM.checked && !timeT.checked && !timeN.checked) {
+        console.log('debe seleccionar algo');
+    }
+});
+
+const textarea = document.getElementById('comments');
+textarea.addEventListener('blur', function () {
+    if (textarea.textContent == false) {
+        this.className = 'error';
+    }
+})
